99f90515210b60eded32eda418143ed70d1a5957
Francesco Mazzoli <f@mazzo.li>
2013-01-29 02:49:51 +0000

    simple names and patterns

--- src/Kant/Syntax.hs
+++ src/Kant/Syntax.hs
@@ -3,69 +3,44 @@
 {-# LANGUAGE DeriveTraversable #-}
 module Kant.Syntax where
 
-import           Control.Applicative (Applicative(..))
-import           Control.Arrow (second)
-import           Control.Monad (ap)
-import           Data.Foldable (Foldable)
-import           Data.Traversable (Traversable)
 
-import           Bound
-import           Bound.Name
-import           Prelude.Extras
+data Id = Id String         -- ^ Original name
+             Int            -- ^ Tag
+    deriving (Eq)
+instance Show Id where
+    show (Id v t) = show (v ++ show t)
 
-
-type Id     = String
 type ConId  = String
 type Module = [Id]
 type Level  = Int
 data QName  = QName Module Id
 
-type ScopeInt t a = Scope (Name Id Int) t a
-type ScopeIntId t = ScopeInt t Id
-type ScopeUnit t a = Scope (Name Id ()) t a
-
 data Decl
     = Val Id                     -- ^ Name
-          (Term Id)              -- ^ Type
-          (ScopeIntId Term)      -- ^ Body, abstracted variables indexed by number,
-                                 --   function itself is 0.
+          Term
+          [Branch]               -- ^ Branches, abstracted variables indexed by
+                                 --   number, function itself is 0.
     | DataType
           Id                     -- ^ Name
-          [TermId]               -- ^ Parameters' types
+          [Term]                 -- ^ Parameters' types
           Level                  -- ^ Resulting level
-          [ScopeIntId ConDecl]   -- ^ The constructors.  Again, parameters by
-                                 --   number, type constructor itself is 0.
+          [ConDecl]              -- ^ Constructors
 
-data ConDecl a = ConDecl ConId [Term a]
+data ConDecl = ConDecl ConId Term
 
-data Term a
-    = Var a
-    | App (Term a) (Term a)
+type Branch = (ConId, [Id], Term)
+
+data Term
+    = Var Id
     | Con ConId
-    | Case (Term a) [CaseBranch a]
     | Set Level
-    | Lambda (Term a)           -- ^ Type of the argument
-             (ScopeUnit Term a)
-    deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
+    | App Term Term
+    | Lambda Id Term Term
+    deriving (Eq, Show)
 
--- | Yet again, variables abstracted by the pattern are indexed by number.
-type CaseBranch a = (ConId, (ScopeInt Term a))
-
-type TermId = Term Id
-
-instance Eq1 Term   where (==#)      = (==)
-instance Ord1 Term  where compare1   = compare
-instance Show1 Term where showsPrec1 = showsPrec
-instance Read1 Term where readsPrec1 = readsPrec
-instance Applicative Term where pure = Var; (<*>) = ap
-
-instance Monad Term where
-    return = Var
-
-    Var v       >>= f = f v
-    App t m     >>= f = App (t >>= f) (m >>= f)
-    Con ci      >>= _ = Con ci
-    Case t bs   >>= f = Case (t >>= f) (map (second (>>>= f)) bs)
-    Set l       >>= _ = Set l
-    Lambda ty t >>= f = Lambda (ty >>= f) (t >>>= f)
-
+subst :: Id -> Term -> Term -> Term
+subst v t m@(Var v')      = if v == v' then t else m
+subst v t (App m n)       = App (subst v t m) (subst v t n)
+subst v t (Lambda v' m n) = Lambda v' (subst v t m)
+                                   (if v == v' then n else subst v t n)
+subst _ _ m               = m
