7c65ee80a70cd96a1b2ed6bedf1f6b3f96fe2a9a
Francesco Mazzoli <f@mazzo.li>
2013-01-29 12:01:07 +0000

    distinguish between names and abstracted variables

--- src/Kant/Syntax.hs
+++ src/Kant/Syntax.hs
@@ -1,22 +1,46 @@
-{-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE DeriveTraversable #-}
-module Kant.Syntax where
+module Kant.Syntax
+    ( Id
+    , rawId
+    , ConId
+    , Level
+    , Module
+    , Decl(..)
+    , ConDecl(..)
+    , Branch(..)
+    , TermT(..)
+    , Term
+    , subst
+    , unRaw
+    , freshen
+    ) where
 
+import           Control.Applicative ((<$>), (<*>))
+import           Data.Maybe (fromMaybe)
 
-data Id = Id String         -- ^ Original name
-             Int            -- ^ Tag
-    deriving (Eq)
-instance Show Id where
-    show (Id v t) = show (v ++ show t)
+import           Control.Monad.State (State, get, put, evalState)
+
+import           Data.Map (Map)
+import qualified Data.Map as Map
+
+type IdTag = Int
+type IdName = String
+
+data IdT a = Id IdName         -- ^ Original name
+                a              -- ^ Tag
+    deriving (Eq, Ord, Show, Functor)
+type IdRaw = IdT ()
+type Id    = IdT IdTag
+
+rawId :: IdName -> IdRaw
+rawId v = Id v ()
 
 type ConId  = String
-type Module = [Id]
 type Level  = Int
-data QName  = QName Module Id
+type Module = [Decl]
 
 data Decl
-    = Val Id                     -- ^ Name
+    = Val IdName                 -- ^ Name
           Term
           [Branch]               -- ^ Branches, abstracted variables indexed by
                                  --   number, function itself is 0.
@@ -28,19 +52,58 @@ data Decl
 
 data ConDecl = ConDecl ConId Term
 
-type Branch = (ConId, [Id], Term)
+data Branch = Branch ConId      -- ^ Matching a constructor
+                     [Id]       -- ^ Abstracting variables
+                     Term
 
-data Term
-    = Var Id
-    | Con ConId
+data TermT id
+    = Var (IdT id)
+    | Name IdName
     | Set Level
-    | App Term Term
-    | Lambda Id Term Term
-    deriving (Eq, Show)
+    | App (TermT id) (TermT id)
+    | Lambda (IdT id) (TermT id) (TermT id)
+    deriving (Eq, Show, Functor)
 
-subst :: Id -> Term -> Term -> Term
+type Term    = TermT IdTag
+type TermRaw = TermT ()
+
+subst :: Eq id => IdT id -> TermT id -> TermT id -> TermT id
 subst v t m@(Var v')      = if v == v' then t else m
 subst v t (App m n)       = App (subst v t m) (subst v t n)
 subst v t (Lambda v' m n) = Lambda v' (subst v t m)
                                    (if v == v' then n else subst v t n)
 subst _ _ m               = m
+
+unRaw :: TermRaw -> Term
+unRaw = go . fmap (const Nothing)
+  where
+    go :: TermT (Maybe IdTag) -> Term
+    go (Var (Id v Nothing))     = Name v
+    go (Var (Id v (Just t)))    = Var (Id v t)
+    go (App m n)                = App (go m) (go n)
+    go (Lambda i@(Id v _) m n)  =
+        Lambda (Id v 0) (go m) (go (subst i (Var (Id v (Just 0))) n))
+    go (Name n)                 = Name n
+    go (Set l)                  = Set l
+
+
+type Fresh a = State (Map String IdTag) a
+
+fresh :: Id -> Fresh Id
+fresh (Id v _) =
+    do m <- get
+       let t' = fromMaybe 0 (Map.lookup v m)
+       put (Map.insert v (t' + 1) m)
+       return (Id v t')
+
+freshen' :: Term -> Fresh Term
+freshen' (App t m)      = App <$> freshen' t <*> freshen' m
+freshen' (Lambda v t m) = do v' <- fresh v
+                             Lambda v' t <$> freshen' (subst v (Var v') m)
+freshen' t              = return t
+
+freshen :: Term -> Term
+freshen t = evalState (freshen' t) Map.empty
+
+-- arrow :: Term
+-- arrow = Var "_->_"
