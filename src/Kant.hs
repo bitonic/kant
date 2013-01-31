9b80d360a41c8580353abcdd31550fac2541893c
Francesco Mazzoli <f@mazzo.li>
2013-01-31 15:33:37 +0000

    a module to rule them all

---
+++ src/Kant.hs
@@ -0,0 +1,13 @@
+module Kant
+    ( module Kant.Parser
+    , module Kant.Lexer
+    , module Kant.Syntax
+    , module Kant.Pretty
+    , module Kant.Reduce
+    ) where
+
+import Kant.Parser
+import Kant.Lexer
+import Kant.Syntax
+import Kant.Pretty
+import Kant.Reduce
