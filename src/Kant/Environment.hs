a27cb12c12eea3be9a9ab205227e48746afbede0
Francesco Mazzoli <f@mazzo.li>
2013-02-01 11:52:01 +0000

    lookups might not succeed

--- src/Kant/Environment.hs
+++ src/Kant/Environment.hs
@@ -3,7 +3,18 @@
 {-# LANGUAGE DeriveTraversable #-}
 -- | Sets up a warm place (cit) to reduce, typecheck, and "reify" things into.
 --   The main hurdle is the multi-level structure of our 'Term', due to bound.
-module Kant.Environment where
+module Kant.Environment
+    ( ItemT(..)
+    , CtxT
+    , Ctx
+    , NestT
+    , Nest
+    , PullT
+    , Pull
+    , EnvT(..)
+    , Env
+    , nestEnv
+    ) where
 
 import           Control.Applicative ((<$>))
 import           Data.Foldable (Foldable)
@@ -19,13 +30,14 @@ data ItemT a
     = Constr Constr Term        -- ^ Constructor with its type
     | TyConstr Term [Constr]    -- ^ Type constructor and associated data
                                 --   constructors
-    | DeclVal Term Term         -- ^ Declared value, type and value
+    | DeclVal Term Term         -- ^ Declared value, type value - the value will
+                                --   be the 'Var' itself for postulated
+                                --   variables.
     | Bound (TermT a)           -- ^ Bound variable, with its type
     deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
 
--- | Given a bound name, returns its type and maybe it's value, if it is a
---   top-level thing.
-type CtxT a = (a -> ItemT a)
+-- | Given a bound name, returns the matching 'ItemT', if present.
+type CtxT a = (a -> Maybe (ItemT a))
 type Ctx = CtxT Id
 
 -- | Nests an 'Id' the required amount of times to turn it into the correct type
@@ -40,10 +52,11 @@ type Pull = PullT Id
 
 -- | Bringing it all together
 data EnvT a = Env
-    { stEnv  :: CtxT a
+    { stCtx  :: CtxT a
     , stNest :: NestT a
     , stPull :: PullT a
     }
+type Env = EnvT Id
 
 -- | To be used when we enter a 'Scope', it adjust the environment functions to
 --   work with the new level of boundness.
@@ -52,10 +65,10 @@ nestEnv :: EnvT a
                                 --   the scope we are entering, same as the one
                                 --   accepted by 'instantiateName' & co.
         -> EnvT (Var (Name Id b) a)
-nestEnv Env{stEnv = env, stNest = nest, stPull = pull} f =
-    Env { stEnv  = \v -> F <$> case v of
-                                   B (Name _ b) -> Bound (f b)
-                                   F v'         -> env v'
+nestEnv Env{stCtx = ctx, stNest = nest, stPull = pull} f =
+    Env { stCtx  = \v -> case v of
+                             B (Name _ b) -> Just (F <$> Bound (f b))
+                             F v'         -> (F <$>) <$> ctx v'
         , stNest = F . nest
         , stPull = \v -> case v of
                              B b  -> name b
