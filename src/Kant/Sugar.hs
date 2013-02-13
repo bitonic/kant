c7e6016c6d4876a563d33eab32c38915f3ade7de
Francesco Mazzoli <f@mazzo.li>
2013-02-13 22:45:59 +0000

    stubs for desugaring

--- src/Kant/Sugar.hs
+++ src/Kant/Sugar.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Kant.Sugar
      ( Id
      , ConId
@@ -11,6 +13,7 @@ module Kant.Sugar
      ) where
 
 import           Kant.Common
+import           Kant.Term
 
 newtype SModule = SModule {unSModule :: [SDecl]}
 
@@ -28,7 +31,40 @@ data STerm
     | SLam [SParam] STerm
     | SApp STerm STerm
     | SArr (Maybe Id) STerm STerm
-    | SLet Id STerm STerm
+-- TODO add this, desugaring to 'case'
+--    | SLet Id STerm STerm STerm
     | SCase Id STerm [SBranch]
 
 type SBranch = (ConId, [Id], STerm)
+
+data DesugarError
+    = RepeatedVariable Id
+
+class Desugar a b where
+    desugar :: a -> Either DesugarError b
+    distill :: b -> a
+
+instance Desugar SDecl Decl where
+    desugar (SVal n pars ty t) = undefined
+    desugar (SPostulate n ty) = undefined
+    desugar (SData c pars l cons) = undefined
+
+    distill (ValD (Val n ty t)) = undefined
+    distill (Postulate n ty) = undefined
+    distill (DataD (Data c pars l cons)) = undefined
+
+instance Desugar STerm (TermT Id) where
+    desugar (SVar n)         = undefined
+    desugar (SType l)        = return (Type l)
+    desugar (SLam pars t)    = undefined
+    desugar (SApp t₁ t₂)     = undefined
+    desugar (SArr mn ty t)   = undefined
+    desugar (SCase n ty brs) = undefined
+
+    distill (Var n) = undefined
+    distill (Type l) = SType l
+    distill (App t₁ t₂) = undefined
+    distill (Lam ty t)  = undefined
+    distill (Case t ty brs) = undefined
+    distill (Constr c tys ts) = undefined
+
