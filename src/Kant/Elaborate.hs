c1c9c3c86eb2b1152b27fe3521fff75f5b7b8de4
Francesco Mazzoli <f@mazzo.li>
2013-03-01 15:43:44 +0000

    eliminators are elaborated all at once

--- src/Kant/Elaborate.hs
+++ src/Kant/Elaborate.hs
@@ -24,7 +24,7 @@ elaborate env₁ (Data tyc cty cons) =
     do tyCheck env₁ cty
        if returnsTy cty
           then do let env₂ = addFree env₁ tyc Nothing (Just cty)
-                  foldrM (\(dc, dty) env₃ -> elaborateCon env₃ tyc cty dc dty)
+                  foldrM (\(dc, dty) env₃ -> elaborateCon env₃ tyc dc dty)
                          env₂ cons
           else throwError (ExpectingTypeCon tyc cty)
   where
@@ -36,42 +36,28 @@ elaborate env₁ (Data tyc cty cons) =
 elaborateCon :: MonadTyCheck m
              => EnvId
              -> ConId           -- ^ Type constructor name
-             -> TermId          -- ^ Type of the tycon
              -> ConId           -- ^ Name of the datacon
              -> TermId          -- ^ Type of the datacon
              -> m EnvId
-elaborateCon env₁ tyc tycty dc ty =
-    do checkDup env₁ dc
-       tyCheck env₁ ty
-       goodTy env₁ ty
-       let env₂ = addFree env₁ dc (Just (buildCanon [] ty)) (Just ty)
-           ety  = elimTy tycty ty
-           et   = buildElim ty
-           env₃ = addFree env₂ elimName (Just et) (Just ety)
-       return (addElim env₃ elimName elimElim)
+elaborateCon env tyc dc ty =
+    do checkDup env dc
+       tyCheck env ty
+       goodTy env ty
+       return (addFree env dc (Just (buildCanon [] ty)) (Just ty))
   where
     goodTy :: (Ord v, Show v, MonadTyCheck m) => Env v -> Term v -> m ()
-    goodTy env (Arr (Abs ty' s)) =
-        do let fvs  = envFreeVs env ty'
-           unless (not (Set.member tyc fvs) || appHead ty' == V (envNest env tyc))
-                  (wrongRecTypePos env₁ dc tyc ty)
-           goodTy (nestEnv env Nothing Nothing) (fromScope s)
-    goodTy env (appV -> AppV t _) =
-        unless (t == V (envNest env tyc)) (expectingTypeData env₁ dc tyc ty)
+    goodTy env' (Arr (Abs ty' s)) =
+        do let fvs  = envFreeVs env' ty'
+           unless (not (Set.member tyc fvs) || appHead ty' == V (envNest env' tyc))
+                  (wrongRecTypePos env dc tyc ty)
+           goodTy (nestEnv env' Nothing Nothing) (fromScope s)
+    goodTy env' (appV -> AppV t _) =
+        unless (t == V (envNest env' tyc)) (expectingTypeData env dc tyc ty)
 
     buildCanon :: [v] -> Term v -> Term v
     buildCanon vs (Arr (Abs ty' s)) =
         Lam (Abs ty' (toScope (buildCanon (B (bindingN s) : map F vs) (fromScope s))))
     buildCanon vs _ = Canon dc (reverse (map V vs))
-
-    buildElim :: Term v -> Term v
-    buildElim _ = Ty
-
-    elimElim :: Elim
-    elimElim _ _ = Ty
-
-    elimTy :: Term v -> Term v -> Term v
-    elimTy _ _ = Ty
 
     elimName = tyc ++ "-Elim"
 
