0d50701b58428cb387faadc69a4abde7a31553c6
Francesco Mazzoli <f@mazzo.li>
2013-04-21 21:56:18 +0100

    finish graph lib

--- src/Data/LGraph.hs
+++ src/Data/LGraph.hs
@@ -1,42 +1,133 @@
+{-# LANGUAGE PatternGuards #-}
+-- | A graph with labelled edges, taken from
+--
+--     /Structuring Depth-First Search Algorithms in Haskell/,
+--     by David King and John Launchbury.
 module Data.LGraph
-    ( LGraph
+    ( Graph
     , Edge
     , empty
+    , build
     , edges
+    , vertices
     , addEdge
-    , sccs
+    , transpose
+    , Tree(..)
+    , Forest
+    , dfs
+    , dff
+    , scc
+    , scc'
+    , SCC(..)
     ) where
+
+import           Control.Monad.ST
+import           Data.List (foldl')
 
 import           Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
-import           Data.HashSet (HashSet)
-import qualified Data.HashSet as HashSet
+import           Data.HashTable.ST.Basic (HashTable)
+import qualified Data.HashTable.ST.Basic as HashTable
 import           Data.Hashable (Hashable(..))
 
-type Successor v l = (v, l)
-newtype LGraph v l = LGraph (HashMap v (HashSet (Successor v l)))
+newtype Graph v l = Graph {unGraph :: HashMap v (HashMap v l)}
 
 type Edge v l = (v, l, v)
 
-empty :: LGraph v l
-empty = LGraph HashMap.empty
+empty :: Graph v l
+empty = Graph HashMap.empty
 
-edges :: LGraph v l -> [Edge v l]
-edges (LGraph gr) = HashMap.foldlWithKey' vEdges [] gr
+build :: (Eq v, Hashable v, Ord l, Hashable l) => [Edge v l] -> Graph v l
+build = foldl' (flip addEdge) empty
+
+edges :: Graph v l -> [Edge v l]
+edges (Graph gr) = HashMap.foldlWithKey' vEdges [] gr
   where
     vEdges es v₁ sucs =
-        HashSet.foldl' (\es' (v₂, l) -> (v₁, l, v₂) : es') [] sucs ++ es
+        HashMap.foldlWithKey' (\es' v₂ l -> (v₁, l, v₂) : es') [] sucs ++ es
 
-addEdge :: (Eq v, Hashable v, Eq l, Hashable l)
-        => Edge v l -> LGraph v l -> LGraph v l
-addEdge (v₁, l, v₂) (LGraph oldGr) =
-    LGraph $ HashMap.insert v₁ (HashSet.insert (v₂, l) sucs) oldGr'
+vertices :: Graph v l -> [v]
+vertices (Graph gr) = HashMap.keys gr
+
+addEdge :: (Eq v, Hashable v, Ord l, Hashable l) => Edge v l -> Graph v l -> Graph v l
+addEdge (v₁, l, v₂) (Graph oldGr) = Graph (HashMap.insert v₁ sucs' oldGr')
   where
     oldGr' = addNode v₂ (addNode v₁ oldGr)
     sucs   = oldGr' HashMap.! v₁
+    sucs'  = case HashMap.lookup v₂ sucs of
+                 Nothing          -> HashMap.insert v₂ l sucs
+                 Just l' | l > l' -> HashMap.insert v₂ l sucs
+                 _                -> sucs
 
     addNode v gr = if HashMap.member v gr then gr
-                   else HashMap.insert v HashSet.empty gr
+                   else HashMap.insert v HashMap.empty gr
 
-sccs :: (Eq v, Hashable v, Eq l, Hashable l) => LGraph v l -> [[Edge v l]]
-sccs = undefined
+reverseG :: Graph v l -> [Edge v l]
+reverseG gr = [(v₂, l, v₁) | (v₁, l, v₂) <- edges gr]
+
+transpose :: (Eq v, Hashable v, Ord l, Hashable l) => Graph v l -> Graph v l
+transpose = build . reverseG
+
+data Tree v l = Node {nodeV :: v, nodeTs :: [(l, Tree v l)]}
+type Forest v l = [Tree v l]
+
+dfs :: (Eq v, Hashable v, Ord l, Hashable l) => Graph v l -> [v] -> Forest v l
+dfs g vs = prune (map (generate g) vs)
+
+dff :: (Eq v, Hashable v, Ord l, Hashable l) => Graph v l -> Forest v l
+dff gr = dfs gr (vertices gr)
+
+scc :: (Eq v, Hashable v, Ord l, Hashable l) => Graph v l -> Forest v l
+scc gr = dfs gr (reverse (postOrd (transpose gr)))
+
+postOrd :: (Eq v, Hashable v, Ord l, Hashable l) => Graph v l -> [v]
+postOrd = concatMap postorder . dff
+  where
+    postorder (Node v ts) = postorderF ts ++ [v]
+    postorderF = concatMap (postorder . snd)
+
+generate :: (Eq v, Hashable v, Ord l, Hashable l) => Graph v l -> v -> Tree v l
+generate (Graph gr) v₁ = Node v₁ (HashMap.foldrWithKey trees [] (gr HashMap.! v₁))
+  where
+    trees v₂ l ts = (l, generate (Graph gr) v₂) : ts
+
+type Set s v = HashTable s v ()
+
+prune :: (Eq v, Hashable v) => Forest v l -> Forest v l
+prune ts = runST $ do m <- HashTable.new; chopTop m ts
+
+chop :: (Eq v, Hashable v)
+     => Set s v
+     -> (a -> v) -> (a -> [(l, Tree v l)]) -> (a -> [(l, Tree v l)] -> a)
+     -> [a] -> ST s [a]
+chop _ _ _ _ [] = return []
+chop m getV getTs putTs (n : us) =
+    do let v = getV n
+       visited <- HashTable.lookup m v
+       case visited of
+           Nothing -> chop m getV getTs putTs us
+           Just () -> do HashTable.insert m v ()
+                         as <- chopRec m (getTs n)
+                         bs <- chop m getV getTs putTs us
+                         return (putTs n as : bs)
+
+chopTop :: (Eq v, Hashable v) => Set s v -> Forest v l -> ST s (Forest v l)
+chopTop m = chop m nodeV nodeTs (\n ts -> n{nodeTs = ts})
+
+chopRec :: (Eq v, Hashable v) => Set s v -> [(l, Tree v l)] -> ST s [(l, Tree v l)]
+chopRec m = chop m (nodeV . snd) (nodeTs . snd) (\(l, n) ts -> (l, n{nodeTs = ts}))
+
+data SCC v l = Acyclic v | Cyclic [Edge v l]
+
+scc' :: (Eq v, Hashable v, Hashable l, Ord l) => Graph v l -> [SCC v l]
+scc' gr = map decode (scc gr)
+  where
+    decode (Node v []) | Just l <- mentionsItself v = Cyclic [(v, l, v)]
+                       | otherwise                  = Acyclic v
+    decode ts          = Cyclic (decode' ts )
+
+    decode' (Node v₁ ts) =
+        [(v₁, l, v₂) | (l, Node v₂ _) <- ts] ++ concatMap (decode' . snd) ts
+
+    mentionsItself v = HashMap.lookup v (unGraph gr HashMap.! v)
+
--- src/Kant/Constraint.hs
+++ src/Kant/Constraint.hs
@@ -10,28 +10,33 @@ import           Data.Foldable (foldrM)
 
 import           Data.Hashable (Hashable(..))
 
-import           Data.LGraph (LGraph)
-import qualified Data.LGraph as LGraph
+import           Data.LGraph (Graph)
+import qualified Data.LGraph as Graph
 
 data ConstrTy = Weak | Strong
     deriving (Eq)
+
+instance Ord ConstrTy where
+    Weak   `compare` Strong = LT
+    Strong `compare` Weak   = GT
+    _      `compare` _      = EQ
 
 instance Hashable ConstrTy where
     hashWithSalt i Weak   = hashWithSalt i True
     hashWithSalt i Strong = hashWithSalt i False
 
-newtype Constrs a = Constrs (LGraph a ConstrTy)
+newtype Constrs a = Constrs (Graph a ConstrTy)
 
 data Constr a = a :<=: a | a :<: a | a :==: a
 
 empty :: Constrs a
-empty = Constrs LGraph.empty
+empty = Constrs Graph.empty
 
 addConstr :: (Eq a, Hashable a) => Constr a -> Constrs a -> Maybe (Constrs a)
 addConstr c (Constrs oldGr) =
     if consistent newGr then Just (Constrs newGr) else Nothing
   where
-    newGr = foldr LGraph.addEdge oldGr (edges c)
+    newGr = foldr Graph.addEdge oldGr (edges c)
 
     edges (r₁ :<=: r₂) = [(r₁, Weak, r₂)]
     edges (r₁ :<:  r₂) = [(r₁, Strong, r₂)]
@@ -40,6 +45,8 @@ addConstr c (Constrs oldGr) =
 addConstrs :: (Eq a, Hashable a) => [Constr a] -> Constrs a -> Maybe (Constrs a)
 addConstrs = flip (foldrM addConstr)
 
-consistent :: (Eq a, Hashable a) => LGraph a ConstrTy -> Bool
-consistent = any strongCycle . LGraph.sccs
-  where strongCycle = any (\(_, cty, _) -> cty == Strong)
+consistent :: (Eq a, Hashable a) => Graph a ConstrTy -> Bool
+consistent = any strongCycle . Graph.scc'
+  where
+    strongCycle (Graph.Acyclic _) = False
+    strongCycle (Graph.Cyclic es) = any (\(_, cty, _) -> cty == Strong) es
