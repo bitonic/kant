2a764ae33e20889f36981a8978d166ac9d29ad36
Francesco Mazzoli <f@mazzo.li>
2013-04-27 21:51:47 +0100

    forgot to add file

---
+++ src/containers.h
@@ -0,0 +1,8 @@
+#ifndef _CONTAINERS_H
+#define _CONTAINERS_H
+
+#include "impossible.h"
+
+#define HMBANG(x, hm) (maybe IMPOSSIBLE("Looking up non-existant item") id (HashMap.lookup (x) (hm)))
+
+#endif
