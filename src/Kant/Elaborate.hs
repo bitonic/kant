efb6d6f74e5c2a1734e2f09d73a7f5e40bcb130d
Francesco Mazzoli <f@mazzo.li>
2013-04-28 14:18:14 +0100

    elaborate with a monad

--- src/Kant/Elaborate.hs
+++ src/Kant/Elaborate.hs
@@ -11,10 +11,12 @@ import           Control.Monad (when, unless)
 import           Data.List (elemIndex)
 import           Data.Maybe (isJust)
 
-import qualified Data.HashSet as HashSet
+import           Control.Monad.Identity (Identity, runIdentity)
 
 import           Bound
 import           Bound.Name
+import qualified Data.HashSet as HashSet
+import           Data.Proxy
 
 import           Kant.ADT
 import           Kant.Common
@@ -50,8 +52,8 @@ instance r ~ Ref => Elaborate (Decl r) where
            -- Create the functions that will form 'Canon's
            mapM (\(dc, dcty) -> elaborateCon tyc dc dcty) dcs
            eltyR <- freshRef
-           let elty  = elimTy tyc tycty dcs eltyR     -- D-elim type
-               eln   = elimName tyc                   -- D-elim name
+           let elty  = runElabM (elimTy tyc tycty dcs eltyR) -- D-elim type
+               eln   = elimName tyc                          -- D-elim name
                -- Function that will form the 'Elim's
                elfun = typedLam (Rewr tyc) elty
            addFreeM eln elty (Just elfun)
@@ -95,90 +97,95 @@ elaborateCon tyc dc ty =
                    and (zipWith (==) pars (map V (reverse vs))))
                   (expectingTypeData dc tyc ty)
 
+type ElabM v a = KMonad (Cursor Proxy) v Identity a
+
+runElabM :: ElabM Id a -> a
+runElabM m = case runIdentity (runKMonad newCurs m) of
+                 Left err     -> IMPOSSIBLE("Got error from KMonad:\n" ++ show err)
+                 Right (x, _) -> x
+
 -- TODO better argument names.
 elimTy :: ConId                 -- ^ Tycon
        -> TermRefId             -- ^ Tycon type
        -> [(ConId, TermRefId)]  -- ^ Constructors
        -> Ref                   -- ^ Reference for the returned Ty
-       -> TermRefId
-elimTy tyc tycty cons ref = targets tycty
+       -> ElabM Id TermRefId
+elimTy tyc tycty cons ref = telescope targetsf tycty
   where
-    -- First scope the arguments of the type constructor
-    targets = telescope targetsf newCurs
-
-    targetsf :: Eq v => CursorP v -> [v] -> TermRef v
-    targetsf curs₁ args =
+    targetsf :: Eq v => [v] -> ElabM v (TermRef v)
+    targetsf args =
         -- Then scope a "motive", which is a predicate on D, so we need to scope
         -- again all the parameters plus an instance of D with those parameters.
-        let targs      = map V args
-            motive     = nestt₁ (mkArr (app (V (cursNest curs₁ tyc) : targs)) (Ty ref))
-            -- The variable that will refer to the motive
-            motiveV    = V (B (Name "P" ()))
-            curs₂      = nestc₂ curs₁
-            -- The arguments to the result of the functions, which will be `P
-            -- args x' where args are the arguments for D and x is the instance
-            -- of D. Note that the variable refers to the thing scoped just
-            -- before the motive: `x'.
-            target     = V (F (B (Name "x" ())))
-        in mkArr (app (V (cursNest curs₁ tyc) : targs))
-                 (mkArr motive (methods curs₂ (map (F . F) args) motiveV target cons))
+        do curs <- getEnv
+           let targs   = map V args
+               motive  = nestt₁ (mkArr (app (V (nest curs tyc) : targs)) (Ty ref))
+               -- The variable that will refer to the motive
+               motiveV = V (B (Name "P" ()))
+               -- The arguments to the result of the functions, which will be `P
+               -- args x' where args are the arguments for D and x is the instance
+               -- of D. Note that the variable refers to the thing scoped just
+               -- before the motive: `x'.
+               target  = V (F (B (Name "x" ())))
+           mkArr (app (V (nest curs tyc) : targs)) <$>
+                 (mkArr motive <$>
+                  nestPM (nestPM (methods (map (F . F) args) motiveV target cons)))
 
     methods :: Eq v
-            => CursorP v
-            -> [v]                  -- Arguments for the tycon
+            => [v]                  -- Arguments for the tycon
             -> TermRef v            -- Quantified motive `P'
             -> TermRef v            -- Target
             -> [(ConId, TermRefId)] -- Constructors
-            -> TermRef v
-    methods _ _ motiveV target [] = app [motiveV, target]
-    methods curs args motiveV target ((dc, dcty) : dcs) =
-        mkArr (method curs args dc dcty motiveV)
-              (methods (nestc₁ curs) (map F args) (nestt₁ motiveV) (nestt₁ target) dcs)
+            -> ElabM v (TermRef v)
+    methods _ motiveV target [] = return (app [motiveV, target])
+    methods args motiveV target ((dc, dcty) : dcs) =
+        mkArr <$> method args dc dcty motiveV
+              <*> nestPM (methods (map F args) (nestt₁ motiveV) (nestt₁ target) dcs)
 
     -- I can't use `telescope' because I need to bump the motiveV each time
     method :: Eq v
-           => CursorP v
-           -> [v]               -- Arguments to the tycon
+           => [v]               -- Arguments to the tycon
            -> ConId             -- Data con
            -> TermRefId         -- Data con type
            -> TermRef v         -- Quantifiend motive `P'
-           -> TermRef v
-    method curs₀ args₀ dc dcty motiveV₀ =
-        let go :: Eq v => CursorP v
-               -> TermRef v        -- Quantified motive `P'
+           -> ElabM v (TermRef v)
+    method args₀ dc dcty motiveV₀ =
+        let go :: Eq v
+               => TermRef v        -- Quantified motive `P'
                -> [v]              -- Args for the tycon
                -> [(v, TermRef v)] -- Args to the datacon, var and type
                -> TermRef v        -- Type of the datacon
-               -> TermRef v
-            go curs motiveV tyargs args (Arr arg s) =
-                mkArr arg
-                      (go (nestc₁ curs) (nestt₁ motiveV) (map F tyargs)
-                          (map (F *** nestt₁) args ++ [(B (bindingN s), nestt₁ arg)])
-                          (fromScope s))
-            go curs motiveV tyargs args (appV -> _) =
-                hyps curs dc motiveV
-                     (app (V (cursNest curs dc) : map V tyargs ++ map (V . fst) args))
-                     args
-        in go curs₀ motiveV₀ args₀ [] (discharge args₀ (cursNest curs₀ <$> dcty))
+               -> ElabM v (TermRef v)
+            go motiveV tyargs args (Arr arg s) =
+                mkArr arg <$>
+                nestPM (go (nestt₁ motiveV) (map F tyargs)
+                           (map (F *** nestt₁) args ++ [(B (bindingN s), nestt₁ arg)])
+                           (fromScope s))
+            go motiveV tyargs args (appV -> _) =
+                do curs <- getEnv
+                   hyps dc motiveV
+                        (app (V (nest curs dc) : map V tyargs ++ map (V . fst) args))
+                        args
+        in do curs <- getEnv
+              go motiveV₀ args₀ [] (discharge args₀ (nest curs <$> dcty))
 
     discharge [] dcty = dcty
     discharge (arg : args) (Arr _ s) = discharge args (instantiate1 (V arg) s)
     discharge _ _ = IMPOSSIBLE("collected arguments don't match type")
 
     hyps :: Eq v
-         => CursorP v
-         -> ConId
+         => ConId
          -> TermRef v            -- Quantified motive `P'
          -> TermRef v            -- Argument for the motive
          -> [(v, TermRef v)]     -- Quantified args of the constructors, with types
-         -> TermRef v
-    hyps _ _ motiveV motiveArg [] = app [motiveV, motiveArg]
-    hyps curs dc motiveV motiveArg ((argv, (appV -> (tyh, _))) : args) =
-        if tyh == V (cursNest curs tyc)
-        then mkArr (app [motiveV, V argv])
-                   (hyps (nestc₁ curs) dc (nestt₁ motiveV) (nestt₁ motiveArg)
-                         (map (F *** nestt₁) args))
-        else hyps curs dc motiveV motiveArg args
+         -> ElabM v (TermRef v)
+    hyps _ motiveV motiveArg [] = return (app [motiveV, motiveArg])
+    hyps dc motiveV motiveArg ((argv, (appV -> (tyh, _))) : args) =
+        do curs <- getEnv
+           if tyh == V (cursNest curs tyc)
+             then mkArr (app [motiveV, V argv]) <$>
+                        nestPM (hyps dc (nestt₁ motiveV) (nestt₁ motiveArg)
+                                     (map (F *** nestt₁) args))
+             else hyps dc motiveV motiveArg args
 
 buildRewr :: Int -> ConId -> [(ConId, TermRefId)] -> Rewr
 -- The `i' is the number of parameters for the tycon, the first 1 for the
@@ -214,12 +221,6 @@ checkDup v =
     do env <- getEnv
        when (isJust (envType env (nest env v))) (duplicateName v)
 
-nestc₁ :: CursorP v -> CursorP (Var (NameId ()) v)
-nestc₁ env = nestCursP env
-
-nestc₂ :: CursorP v -> CursorP (Var (NameId ()) (Var (NameId ()) v))
-nestc₂ = nestc₁ . nestc₁
-
 nestt₁ :: Functor f => f a -> f (Var b a)
 nestt₁ = fmap F
 
@@ -232,24 +233,24 @@ instDummy s = instantiate1 (V "dummy") s
 -- | Provided with a @(x1 : S1) -> ... -> (xn : Sn) -> T@ returns a
 --   @(x1 : S1) -> ... -> (xn : Sn) -> f [x1..xn]@.
 telescope :: Eq v
-          => (forall a. Eq a => CursorP a -> [a] -> TermRef a)
-          -> CursorP v -> TermRef v -> TermRef v
-telescope f env' = go env' []
+          => (forall a. Eq a => [a] -> ElabM a (TermRef a))
+          -> TermRef v -> ElabM v (TermRef v)
+telescope f = go []
   where
-    go :: Eq v => CursorP v -> [v] -> TermRef v -> TermRef v
-    go curs vs (Arr ty s) =
-        Arr ty (toScope (go (nestc₁ curs) (B (bindingN s) : map F vs) (fromScope s)))
-    go curs vs _ = f curs (reverse vs)
+    go :: Eq v => [v] -> TermRef v -> ElabM v (TermRef v)
+    go vs (Arr ty s) =
+        Arr ty <$> (toScope <$> nestPM (go (B (bindingN s) : map F vs) (fromScope s)))
+    go vs _ = f (reverse vs)
 
 -- | Provided with a @A = (x1 : S1) -> ... -> (xn : Sn) -> T@ returns a
 --   @(\x1 .. xn  => f [x1..xn]) : A@.
 typedLam :: (forall a. [TermRef a] -> TermRef a) -> TermRefId -> TermRefId
-typedLam f ty = Ann ty (go newCurs [] ty)
+typedLam f ty = Ann ty (runElabM (go [] ty))
   where
-    go :: Eq v => CursorP v -> [v] -> TermRef v -> TermRef v
-    go env vs (Arr _ s) =
-        Lam (toScope (go (nestc₁ env) (B (bindingN s) : map F vs) (fromScope s)))
-    go _ vs _ = f (map V (reverse vs))
+    go :: Eq v => [v] -> TermRef v -> ElabM v (TermRef v)
+    go vs (Arr _ s) =
+        Lam <$> (toScope <$> nestPM (go (B (bindingN s) : map F vs) (fromScope s)))
+    go vs _ = return (f (map V (reverse vs)))
 
 instance r ~ Ref => Elaborate (Module r) where
     elaborate = go [] . unModule
