6c03a727bd36af8fa6879009e94e536a4186de2f
Francesco Mazzoli <f@mazzo.li>
2013-07-21 16:00:53 +0200

    occurring metas

---
+++ src/Language/Bertus/Occurs.hs
@@ -0,0 +1,16 @@
+module Language.Bertus.Occurs where
+
+import Data.Data (Data)
+import Data.Generics.Schemes (listify)
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Language.Bertus.Tm
+
+metas :: Data a => a -> Set Meta
+metas = Set.fromList . listify (const True)
+
+occurs :: Data a => Meta -> a -> Bool
+occurs mv = Set.member mv . metas
+
